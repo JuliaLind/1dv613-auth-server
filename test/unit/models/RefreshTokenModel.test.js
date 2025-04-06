01afa626d156f54b3132ff36c4989b96c628d991
Julia Lind <jl225vf@student.lnu.se>
2025-04-06 14:32:21 +0200

    Add more tests for RefreshTokenModel

--- test/unit/models/RefreshTokenModel.test.js
+++ test/unit/models/RefreshTokenModel.test.js
@@ -6,7 +6,6 @@ import sinon from 'sinon'
 
 import { RefreshTokenModel } from '../../../src/models/RefreshTokenModel.js'
 
-
 const expect = chai.expect
 
 describe('RefreshTokenModel', () => {
@@ -17,6 +16,11 @@ describe('RefreshTokenModel', () => {
   it('newJti', async () => {
     const refreshTokenDoc = {
       _id: {
+        /**
+         * Returns the stringified id of the token.
+         *
+         * @returns {string} The stringified id of the token.
+         */
         toString: () => '123'
       }
     }
@@ -28,23 +32,64 @@ describe('RefreshTokenModel', () => {
     expect(RefreshTokenModel.create).to.have.been.calledOnce
   })
 
+  it('authenticate OK', async () => {
+    RefreshTokenModel.expireChain = sinon.stub().resolves()
+    const token = {
+      expired: false
+    }
+    RefreshTokenModel.findById = sinon.stub().resolves(token)
+
+    const res = await RefreshTokenModel.authenticate('456')
+    expect(res).to.equal(token)
+  })
+
+  it('authenticate Not OK', async () => {
+    RefreshTokenModel.expireChain = sinon.stub().resolves()
+    const token = {
+      expired: true
+    }
+    RefreshTokenModel.findById = sinon.stub().resolves(token)
+
+    await expect(RefreshTokenModel.authenticate('456')).to.be.rejectedWith('Token expired')
+  })
+
+  it('expireById OK', async () => {
+    const token = {
+      expire: sinon.stub().resolves(null)
+    }
+    sinon.stub(RefreshTokenModel, 'findById').resolves(token)
+
+    await RefreshTokenModel.expireById('456')
+    expect(token.expire).to.have.been.calledOnce
+  })
+
   it('expireChain OK', async () => {
     const token3 = {
       _id: {
+        /**
+         * Returns the stringified id of the token.
+         *
+         * @returns {string} The stringified id of the token.
+         */
         toString: () => '789'
       },
-      expire: sinon.stub().resolves(null),
+      expire: sinon.stub().resolves(null)
     }
 
     const token2 = {
       _id: {
+        /**
+         * Returns the stringified id of the token.
+         *
+         * @returns {string} The stringified id of the token.
+         */
         toString: () => '456'
       },
-      expire: sinon.stub().resolves(token3._id),
+      expire: sinon.stub().resolves(token3._id)
     }
 
     const token1 = {
-      expire: sinon.stub().resolves(token2._id),
+      expire: sinon.stub().resolves(token2._id)
     }
     sinon.stub(RefreshTokenModel, 'findById').callsFake((id) => {
       if (id === token2._id) return token2
