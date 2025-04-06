cfdf9ced6d0aed5d90d3cae02ffcfd7f45057d55
Julia Lind <jl225vf@student.lnu.se>
2025-04-06 20:23:47 +0200

    Implement JwtService.encode

---
+++ src/services/JwtService.js
@@ -0,0 +1,42 @@
+/**
+ * Contains the JsonWebToken class.
+ *
+ * @version 1.0.0
+ */
+
+import jwt from 'jsonwebtoken'
+
+/**
+ * Exposes methods for working with JSON Web Tokens (JWTs).
+ */
+export class JwtService {
+  /**
+   * Encodes user information into a JSON Web Token (JWT) payload.
+   *
+   * @param {object} payload - the payload of the JWT.
+   * @param {string} secret - The secret key used for signing the JWT.
+   * @param {string|number} expiresIn - The expiration time for the JWT, specified in seconds or as a string describing a time span (e.g., '1d', '2h') using the vercel/ms library.
+   * @param {string} algorithm - the alogrithm to use for signing the JWT - default is RS256 for assymetric key.
+   * @returns {Promise<string>} A Promise that resolves to the generated JWT.
+   */
+  static async encode (payload, secret, expiresIn, algorithm = 'RS256') {
+    return new Promise((resolve, reject) => {
+      jwt.sign(
+        payload,
+        secret,
+        {
+          algorithm,
+          expiresIn
+        },
+        (error, token) => {
+          if (error) {
+            reject(error)
+            return
+          }
+
+          resolve(token)
+        }
+      )
+    })
+  }
+}
