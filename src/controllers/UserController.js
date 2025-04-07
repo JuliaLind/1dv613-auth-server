1cedba826dd1f407bc56c2d4153807afabef56ff
Julia Lind <jl225vf@student.lnu.se>
2025-04-07 19:38:02 +0200

    Implement login method

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -120,4 +120,26 @@ export class UserController {
       next(error)
     }
   }
+
+  /**
+   * Checks the user credentials agains db
+   * and generates a new jwt token.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async login (req, res, next) {
+    const { username, password } = req.body
+
+    try {
+      // authenticate, will throw error if user does not exist or invalid password
+      const user = await UserModel.authenticate(username, password)
+      const result = await this.tokenService.newTokenPair(user)
+
+      res.status(201).json(result.tokens)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
--- test/unit/controllers/UserController.login.test.js
+++ test/unit/controllers/UserController.login.test.js
@@ -1,85 +1,85 @@
-// /* global afterEach */
-// /* eslint-disable no-unused-expressions */
+/* global afterEach */
+/* eslint-disable no-unused-expressions */
 
-// import chai from 'chai'
-// import sinon from 'sinon'
-// import { TokenService } from '../../../src/services/TokenService.js'
-// import { UserModel } from '../../../src/models/UserModel.js'
-// import { UserController } from '../../../src/controllers/UserController.js'
+import chai from 'chai'
+import sinon from 'sinon'
+import { TokenService } from '../../../src/services/TokenService.js'
+import { UserModel } from '../../../src/models/UserModel.js'
+import { UserController } from '../../../src/controllers/UserController.js'
 
-// const expect = chai.expect
+const expect = chai.expect
 
-// describe('UserController.login', () => {
-//   afterEach(() => {
-//     sinon.restore()
-//   })
+describe('UserController.login', () => {
+  afterEach(() => {
+    sinon.restore()
+  })
 
-//   const username = 'julia'
-//   const password = 'mypassword'
+  const username = 'julia'
+  const password = 'mypassword'
 
-//   const user = {
-//     username
-//   }
+  const user = {
+    username
+  }
 
-//   const tokens = {
-//     accessToken: 'myAccessToken',
-//     refreshToken: 'myRefreshToken'
-//   }
+  const tokens = {
+    accessToken: 'myAccessToken',
+    refreshToken: 'myRefreshToken'
+  }
 
-//   const jti = '123'
+  const jti = '123'
 
-//   it('Ok', async () => {
-//     const body = {
-//       username,
-//       password
-//     }
+  it('Ok', async () => {
+    const body = {
+      username,
+      password
+    }
 
-//     sinon.stub(UserModel, 'authenticate').resolves(user)
-//     const tokenService = new TokenService()
-//     tokenService.newTokenPair = sinon.stub().resolves({
-//       tokens,
-//       jti
-//     })
+    sinon.stub(UserModel, 'authenticate').resolves(user)
+    const tokenService = new TokenService()
+    tokenService.newTokenPair = sinon.stub().resolves({
+      tokens,
+      jti
+    })
 
-//     const req = {
-//       body
-//     }
-//     const res = {
-//       status: sinon.stub().returnsThis(),
-//       json: sinon.stub()
-//     }
-//     const next = sinon.stub()
+    const req = {
+      body
+    }
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub()
+    }
+    const next = sinon.stub()
 
-//     const userController = new UserController(tokenService)
-//     await userController.login(req, res, next)
-//     expect(res.status).to.have.been.calledWith(201)
-//     expect(res.json).to.have.been.calledWith(tokens)
-//     expect(next).not.to.have.been.called
-//     expect(UserModel.authenticate).to.have.been.calledWith(username, password)
-//     expect(tokenService.newTokenPair).to.have.been.calledWith(user)
-//   })
+    const userController = new UserController(tokenService)
+    await userController.login(req, res, next)
+    expect(res.status).to.have.been.calledWith(201)
+    expect(res.json).to.have.been.calledWith(tokens)
+    expect(next).not.to.have.been.called
+    expect(UserModel.authenticate).to.have.been.calledWith(username, password)
+    expect(tokenService.newTokenPair).to.have.been.calledWith(user)
+  })
 
-//   it('not ok, faied authentication', async () => {
-//     const body = {
-//       username,
-//       password
-//     }
+  it('not ok, faied authentication', async () => {
+    const body = {
+      username,
+      password
+    }
 
-//     sinon.stub(UserModel, 'authenticate').rejects(new Error('Credentials invalid or not provided.'))
+    sinon.stub(UserModel, 'authenticate').rejects(new Error('Credentials invalid or not provided.'))
 
-//     const req = {
-//       body
-//     }
-//     const res = {
-//       status: sinon.stub().returnsThis(),
-//       json: sinon.stub()
-//     }
-//     const next = sinon.stub()
+    const req = {
+      body
+    }
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub()
+    }
+    const next = sinon.stub()
 
-//     const userController = new UserController()
-//     await userController.login(req, res, next)
-//     expect(res.status).not.to.have.been.called
-//     expect(res.json).not.to.have.been.called
-//     expect(next).to.have.been.calledWith(sinon.match.instanceOf(Error))
-//   })
-// })
+    const userController = new UserController()
+    await userController.login(req, res, next)
+    expect(res.status).not.to.have.been.called
+    expect(res.json).not.to.have.been.called
+    expect(next).to.have.been.calledWith(sinon.match.instanceOf(Error))
+  })
+})
