9dc20dc15bbeb7c30ca43fcab2ab59009ea72ad7
Julia Lind <jl225vf@student.lnu.se>
2025-05-07 21:02:07 +0200

    Remove debugging code

--- src/services/TokenService.js
+++ src/services/TokenService.js
@@ -35,7 +35,6 @@ export class TokenService {
     const payload = {
       jti
     }
-    console.log('[DEBUG] Refresh key length:', process.env.REFRESH_TOKEN_KEY?.length)
     const refreshToken = await JwtService.encode(payload, process.env.REFRESH_TOKEN_KEY, process.env.REFRESH_TOKEN_LIFE, 'HS256')
 
     return [refreshToken, jti]
